5ba5f2121cc1fb547377643f254f9a1e500ee6ae
Richard Patel <me@terorie.dev>
2021-01-26 19:11:00 +0100

    Worker token support

--- management_grpc_web_pb.js
+++ management_grpc_web_pb.js
@@ -77,13 +77,13 @@ proto.od2_network.hive.ManagementPromiseClient =
  * @const
  * @type {!grpc.web.MethodDescriptor<
  *   !proto.od2_network.hive.CreateWorkerTokenRequest,
- *   !proto.od2_network.hive.WorkerToken>}
+ *   !proto.od2_network.hive.CreateWorkerTokenResponse>}
  */
 const methodDescriptor_Management_CreateWorkerToken = new grpc.web.MethodDescriptor(
   '/od2_network.hive.Management/CreateWorkerToken',
   grpc.web.MethodType.UNARY,
   proto.od2_network.hive.CreateWorkerTokenRequest,
-  proto.od2_network.hive.WorkerToken,
+  proto.od2_network.hive.CreateWorkerTokenResponse,
   /**
    * @param {!proto.od2_network.hive.CreateWorkerTokenRequest} request
    * @return {!Uint8Array}
@@ -91,7 +91,7 @@ const methodDescriptor_Management_CreateWorkerToken = new grpc.web.MethodDescrip
   function(request) {
     return request.serializeBinary();
   },
-  proto.od2_network.hive.WorkerToken.deserializeBinary
+  proto.od2_network.hive.CreateWorkerTokenResponse.deserializeBinary
 );
 
 
@@ -99,10 +99,10 @@ const methodDescriptor_Management_CreateWorkerToken = new grpc.web.MethodDescrip
  * @const
  * @type {!grpc.web.AbstractClientBase.MethodInfo<
  *   !proto.od2_network.hive.CreateWorkerTokenRequest,
- *   !proto.od2_network.hive.WorkerToken>}
+ *   !proto.od2_network.hive.CreateWorkerTokenResponse>}
  */
 const methodInfo_Management_CreateWorkerToken = new grpc.web.AbstractClientBase.MethodInfo(
-  proto.od2_network.hive.WorkerToken,
+  proto.od2_network.hive.CreateWorkerTokenResponse,
   /**
    * @param {!proto.od2_network.hive.CreateWorkerTokenRequest} request
    * @return {!Uint8Array}
@@ -110,7 +110,7 @@ const methodInfo_Management_CreateWorkerToken = new grpc.web.AbstractClientBase.
   function(request) {
     return request.serializeBinary();
   },
-  proto.od2_network.hive.WorkerToken.deserializeBinary
+  proto.od2_network.hive.CreateWorkerTokenResponse.deserializeBinary
 );
 
 
@@ -119,9 +119,9 @@ const methodInfo_Management_CreateWorkerToken = new grpc.web.AbstractClientBase.
  *     request proto
  * @param {?Object<string, string>} metadata User defined
  *     call metadata
- * @param {function(?grpc.web.Error, ?proto.od2_network.hive.WorkerToken)}
+ * @param {function(?grpc.web.Error, ?proto.od2_network.hive.CreateWorkerTokenResponse)}
  *     callback The callback function(error, response)
- * @return {!grpc.web.ClientReadableStream<!proto.od2_network.hive.WorkerToken>|undefined}
+ * @return {!grpc.web.ClientReadableStream<!proto.od2_network.hive.CreateWorkerTokenResponse>|undefined}
  *     The XHR Node Readable Stream
  */
 proto.od2_network.hive.ManagementClient.prototype.createWorkerToken =
@@ -140,7 +140,7 @@ proto.od2_network.hive.ManagementClient.prototype.createWorkerToken =
  *     request proto
  * @param {?Object<string, string>} metadata User defined
  *     call metadata
- * @return {!Promise<!proto.od2_network.hive.WorkerToken>}
+ * @return {!Promise<!proto.od2_network.hive.CreateWorkerTokenResponse>}
  *     Promise that resolves to the response
  */
 proto.od2_network.hive.ManagementPromiseClient.prototype.createWorkerToken =
@@ -150,6 +150,246 @@ proto.od2_network.hive.ManagementPromiseClient.prototype.createWorkerToken =
       request,
       metadata || {},
       methodDescriptor_Management_CreateWorkerToken);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.od2_network.hive.ListWorkerTokensRequest,
+ *   !proto.od2_network.hive.ListWorkerTokensResponse>}
+ */
+const methodDescriptor_Management_ListWorkerTokens = new grpc.web.MethodDescriptor(
+  '/od2_network.hive.Management/ListWorkerTokens',
+  grpc.web.MethodType.UNARY,
+  proto.od2_network.hive.ListWorkerTokensRequest,
+  proto.od2_network.hive.ListWorkerTokensResponse,
+  /**
+   * @param {!proto.od2_network.hive.ListWorkerTokensRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.od2_network.hive.ListWorkerTokensResponse.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.od2_network.hive.ListWorkerTokensRequest,
+ *   !proto.od2_network.hive.ListWorkerTokensResponse>}
+ */
+const methodInfo_Management_ListWorkerTokens = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.od2_network.hive.ListWorkerTokensResponse,
+  /**
+   * @param {!proto.od2_network.hive.ListWorkerTokensRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.od2_network.hive.ListWorkerTokensResponse.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.od2_network.hive.ListWorkerTokensRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.od2_network.hive.ListWorkerTokensResponse)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.od2_network.hive.ListWorkerTokensResponse>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.od2_network.hive.ManagementClient.prototype.listWorkerTokens =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/od2_network.hive.Management/ListWorkerTokens',
+      request,
+      metadata || {},
+      methodDescriptor_Management_ListWorkerTokens,
+      callback);
+};
+
+
+/**
+ * @param {!proto.od2_network.hive.ListWorkerTokensRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.od2_network.hive.ListWorkerTokensResponse>}
+ *     Promise that resolves to the response
+ */
+proto.od2_network.hive.ManagementPromiseClient.prototype.listWorkerTokens =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/od2_network.hive.Management/ListWorkerTokens',
+      request,
+      metadata || {},
+      methodDescriptor_Management_ListWorkerTokens);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.od2_network.hive.RevokeWorkerTokenRequest,
+ *   !proto.od2_network.hive.RevokeWorkerTokenResponse>}
+ */
+const methodDescriptor_Management_RevokeWorkerToken = new grpc.web.MethodDescriptor(
+  '/od2_network.hive.Management/RevokeWorkerToken',
+  grpc.web.MethodType.UNARY,
+  proto.od2_network.hive.RevokeWorkerTokenRequest,
+  proto.od2_network.hive.RevokeWorkerTokenResponse,
+  /**
+   * @param {!proto.od2_network.hive.RevokeWorkerTokenRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.od2_network.hive.RevokeWorkerTokenResponse.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.od2_network.hive.RevokeWorkerTokenRequest,
+ *   !proto.od2_network.hive.RevokeWorkerTokenResponse>}
+ */
+const methodInfo_Management_RevokeWorkerToken = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.od2_network.hive.RevokeWorkerTokenResponse,
+  /**
+   * @param {!proto.od2_network.hive.RevokeWorkerTokenRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.od2_network.hive.RevokeWorkerTokenResponse.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.od2_network.hive.RevokeWorkerTokenRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.od2_network.hive.RevokeWorkerTokenResponse)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.od2_network.hive.RevokeWorkerTokenResponse>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.od2_network.hive.ManagementClient.prototype.revokeWorkerToken =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/od2_network.hive.Management/RevokeWorkerToken',
+      request,
+      metadata || {},
+      methodDescriptor_Management_RevokeWorkerToken,
+      callback);
+};
+
+
+/**
+ * @param {!proto.od2_network.hive.RevokeWorkerTokenRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.od2_network.hive.RevokeWorkerTokenResponse>}
+ *     Promise that resolves to the response
+ */
+proto.od2_network.hive.ManagementPromiseClient.prototype.revokeWorkerToken =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/od2_network.hive.Management/RevokeWorkerToken',
+      request,
+      metadata || {},
+      methodDescriptor_Management_RevokeWorkerToken);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.od2_network.hive.RevokeAllWorkerTokensRequest,
+ *   !proto.od2_network.hive.RevokeAllWorkerTokensResponse>}
+ */
+const methodDescriptor_Management_RevokeAllWorkerTokens = new grpc.web.MethodDescriptor(
+  '/od2_network.hive.Management/RevokeAllWorkerTokens',
+  grpc.web.MethodType.UNARY,
+  proto.od2_network.hive.RevokeAllWorkerTokensRequest,
+  proto.od2_network.hive.RevokeAllWorkerTokensResponse,
+  /**
+   * @param {!proto.od2_network.hive.RevokeAllWorkerTokensRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.od2_network.hive.RevokeAllWorkerTokensResponse.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.od2_network.hive.RevokeAllWorkerTokensRequest,
+ *   !proto.od2_network.hive.RevokeAllWorkerTokensResponse>}
+ */
+const methodInfo_Management_RevokeAllWorkerTokens = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.od2_network.hive.RevokeAllWorkerTokensResponse,
+  /**
+   * @param {!proto.od2_network.hive.RevokeAllWorkerTokensRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.od2_network.hive.RevokeAllWorkerTokensResponse.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.od2_network.hive.RevokeAllWorkerTokensRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.od2_network.hive.RevokeAllWorkerTokensResponse)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.od2_network.hive.RevokeAllWorkerTokensResponse>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.od2_network.hive.ManagementClient.prototype.revokeAllWorkerTokens =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/od2_network.hive.Management/RevokeAllWorkerTokens',
+      request,
+      metadata || {},
+      methodDescriptor_Management_RevokeAllWorkerTokens,
+      callback);
+};
+
+
+/**
+ * @param {!proto.od2_network.hive.RevokeAllWorkerTokensRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.od2_network.hive.RevokeAllWorkerTokensResponse>}
+ *     Promise that resolves to the response
+ */
+proto.od2_network.hive.ManagementPromiseClient.prototype.revokeAllWorkerTokens =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/od2_network.hive.Management/RevokeAllWorkerTokens',
+      request,
+      metadata || {},
+      methodDescriptor_Management_RevokeAllWorkerTokens);
 };
 
 
